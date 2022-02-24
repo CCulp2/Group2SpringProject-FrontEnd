b13aec8b5500bfd131eee97ac3db0aaa24a85227
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-02-24 18:48:37 -0500

    Stub out grid for registration form
    General idea of where everything is going within a grid.

--- capstone-react-app/src/Components/Register.jsx
+++ capstone-react-app/src/Components/Register.jsx
@@ -1,7 +1,37 @@
+import Grid from '@mui/material'
+import Item from '@mui/material'
 import React from 'react'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <Grid container spacing="2">
+      <Grid item xs={8}>
+        <Item>{/*Username Form*/}</Item>
+      </Grid>
+      <Grid item xs={2}>
+        <Item>{/* Checkmark */}</Item>
+      </Grid>
+      <Grid item xs={8}>
+        <Item>{/*Password Form*/}</Item>
+      </Grid>
+      <Grid item xs={2}>
+        <Item>{/* Checkmark */}</Item>
+      </Grid>
+      <Grid item xs={8}>
+        <Item>{/*Password Recheck*/}</Item>
+      </Grid>
+      <Grid item xs={2}>
+        <Item>{/* Checkmark */}</Item>
+      </Grid>
+      <Grid item xs={8}>
+        <Item>{/*Address Form*/}</Item>
+      </Grid>
+      <Grid item xs={2}>
+        <Item>{/* Checkmark */}</Item>
+      </Grid>
+      <Grid item xs={12}>
+        <Item>{/*Send Button*/}</Item>
+      </Grid>
+    </Grid>
   )
 }
