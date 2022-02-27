e39eb3205456e366eb6649af2a3e42771cf98561
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-02-27 15:12:45 -0500

    Form validator added to Register
    Added the validator to Register as well.

--- capstone-react-app/src/Components/Register.jsx
+++ capstone-react-app/src/Components/Register.jsx
@@ -1,13 +1,28 @@
 import { Box, TextField, Button, Grid } from "@mui/material";
+import { useFormik } from 'formik';
 import React from "react";
 
 export default function Register() {
+  const formik = useFormik({
+    initialValues: {
+      userName: '',
+      password: '',
+      address: ''
+    },
+    onSubmit: values => {
+    alert(JSON.stringify(values, null, 2));
+    }    
+  });
   return (
     <Box sx={{ flexgrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={8}>
           {/* Textfield for Username */}
-          <TextField id="outlined-basic" label="Username" varient="outlined" />
+          <TextField 
+            id="userName"
+            name="userName"
+            label="Username" 
+            varient="" />
         </Grid>
         <Grid item xs={2}>
           {/* Checkmark */}
