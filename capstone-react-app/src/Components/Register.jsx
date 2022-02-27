b25ee01484ace54836a00c027f995cc0f9a2bdf5
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-02-27 16:00:16 -0500

    Loading in Yup
    Installed Yup for validation with Formik

--- capstone-react-app/src/Components/Register.jsx
+++ capstone-react-app/src/Components/Register.jsx
@@ -1,58 +1,49 @@
 import { Box, TextField, Button, Grid } from "@mui/material";
-import { useFormik } from 'formik';
+import { useFormik } from "formik";
 import React from "react";
 
 export default function Register() {
   const formik = useFormik({
     initialValues: {
-      userName: '',
-      password: '',
-      address: ''
+      userName: "",
+      password: "",
+      address: "",
     },
-    onSubmit: values => {
-    alert(JSON.stringify(values, null, 2));
-    }    
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
   });
   return (
-    <Box sx={{ flexgrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
+    <div className="registerForm">
           {/* Textfield for Username */}
-          <TextField 
+          <TextField
             id="userName"
             name="userName"
-            label="Username" 
-            varient="" />
-        </Grid>
-        <Grid item xs={2}>
-          {/* Checkmark */}
-        </Grid>
+            label="Username"
+            varient="outlined"
+          />
 
-        <Grid item xs={8}>
-          <TextField id="ounlined-basic" label="Password" variant="outlined" />
-        </Grid>
-        <Grid item xs={2}>
-          {/* Checkmark */}
-        </Grid>
-        
-        <Grid item xs={8}>
-          <TextField id="ounlined-basic" label="Password" variant="outlined" />
-        </Grid>
-        <Grid item xs={2}>
-          {/* Checkmark */}
-        </Grid>
-        
-        <Grid item xs={8}>
-          <TextField id="outlined-basic" label="Address" varient="outlined" />
-        </Grid>
-        <Grid item xs={2}>
-          {/* Checkmark */}
-        </Grid>
-        
-        <Grid item xs={12}>
+          <TextField
+            id="password"
+            name="password"
+            label="Password"
+            variant="outlined"
+          />
+
+
+          <TextField
+            id="passwordRecheck"
+            label="passwordRecheck"
+            name="passwordRecheck"
+            variant="outlined"
+          />
+          <TextField
+            id="address"
+            label="address"
+            name="address"
+            varient="outlined"
+          />
           <Button variant="contained">Submit</Button>
-        </Grid>
-      </Grid>
-    </Box>
+    </div>
   );
 }
