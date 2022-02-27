75ea4b49163983737871badb7dbd3e420ff3677f
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-02-27 15:11:54 -0500

    Create Login, Form validation
    Added a login form so I could test the register form. Still only showing alerts. Added in props for form.

---
+++ capstone-react-app/src/Components/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useFormik } from 'formik';
+import { TextField, Button } from '@mui/material';
+
+export default function Login() {
+    const formik = useFormik({
+        initialValues: {
+            userName: '',
+            password: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+  return (
+    <div className='loginForm'>
+        <TextField
+            fullWidth
+            id='userName'
+            name='userName'
+            label='Username'
+            value={formik.values.userName}
+            onChange={formik.handleChange} />
+        <TextField
+            fullWidth
+            id="password"
+            name='password'
+            label='Password'    
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            />
+            <Button color='primary' varient='contained' fullWidth type='submit'>Log In</Button>                    
+    </div>
+  )
+}
