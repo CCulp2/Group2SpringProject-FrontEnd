effc12d286ac0b0e992cf9edb1ff5161668379aa
anazworth <98629900+anazworth@users.noreply.github.com>
2022-02-27 19:24:37 -0500

    Added data.js APN
    Just realized I didnt create a branch lol

---
+++ capstone-react-app/src/data.js
@@ -0,0 +1,26 @@
+export const MensTops = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1620456456327-264dbf934b06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODJ8fGJsYWNrJTIwdCUyMHNoaXJ0JTIwbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+        title: "Essential Crewneck Black",
+        desc: "Our essential crewneck t-shirt, in a color that goes with everything. Wear it everyday, with everything.",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1620456456327-264dbf934b06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODJ8fGJsYWNrJTIwdCUyMHNoaXJ0JTIwbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+        title: "Essential Crewneck Black",
+        desc: "Our essential crewneck t-shirt, in a color that goes with everything. Wear it everyday, with everything.",
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1620456456327-264dbf934b06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODJ8fGJsYWNrJTIwdCUyMHNoaXJ0JTIwbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+        title: "Essential Crewneck Black",
+        desc: "Our essential crewneck t-shirt, in a color that goes with everything. Wear it everyday, with everything.",
+    },
+    {
+        id: 4,
+        img: "https://images.unsplash.com/photo-1620456456327-264dbf934b06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODJ8fGJsYWNrJTIwdCUyMHNoaXJ0JTIwbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+        title: "Essential Crewneck Black",
+        desc: "Our essential crewneck t-shirt, in a color that goes with everything. Wear it everyday, with everything.",
+    },
+]
