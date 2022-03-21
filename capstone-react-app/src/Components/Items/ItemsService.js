400a6b32a4bb9ab3dce48d9296ffc039e7803c83
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-03-21 14:57:14 -0400

    Added getItem()
    Added getItem() to get an individual item from the db

--- capstone-react-app/src/Components/Items/ItemsService.js
+++ capstone-react-app/src/Components/Items/ItemsService.js
@@ -1,9 +1,30 @@
-const PRODUCT_API_URL = 'http://127.0.0.1:8080/api/v1/product'
+const PRODUCT_API_URL = "http://127.0.0.1:8080/api/v1/product/";
 
 export async function getAllItems() {
-    return fetch(PRODUCT_API_URL).then(res => {
-        return res.json()
-    }).then(data => {
-        return data;
+  return fetch(PRODUCT_API_URL)
+    .then((res) => {
+      return res.json();
     })
-};
+    .then((data) => {
+      return data;
+    });
+}
+
+export async function getItem(id) {
+  return fetch(PRODUCT_API_URL + id)
+  .then((res => {
+      return res.json();
+  }))
+  .then((data) => {
+      return data;
+  });
+}
+
+export async function addItem(item) {
+    fetch(PRODUCT_API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
