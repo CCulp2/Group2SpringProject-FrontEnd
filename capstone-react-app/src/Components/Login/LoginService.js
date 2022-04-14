c82fe1a1e5f04f8f3a429afd5ff14d2ac6002acc
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-04-14 13:12:49 -0400

    ShoppingCartLogic

--- capstone-react-app/src/Components/Login/LoginService.js
+++ capstone-react-app/src/Components/Login/LoginService.js
@@ -2,17 +2,18 @@ import { API_URL_BASE } from "../APIUrlBase"
 import { storeJWT } from "./JWTService";
 
 
-const LOGIN_BASE = API_URL_BASE + '/login'
+const LOGIN_BASE = API_URL_BASE + '/customer/login'
 const abort = new AbortController();
 
 export async function LoginSubmit(username, password) {
-    return fetch(LOGIN_BASE, {
+    return fetch(LOGIN_BASE + "?username=" + username + "&password=" + password, {
         method: "post",
-        headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-        username: username,
-        password: password
-    })
-    }).then(res => storeJWT(res));
+        headers: { 'Content-Type': 'application/json'},
+        body: {
+        "username": username,
+        "password": password
+    }
+    }).then(res => //storeJWT(res));
+    console.log(res.json()));
     
 }
