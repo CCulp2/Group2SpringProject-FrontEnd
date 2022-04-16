735189a13f626ead1309a8b865a6a5fe579afd9d
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-04-16 02:30:02 -0400

    Base URL is changed to Heroku

--- capstone-react-app/src/Components/APIUrlBase.js
+++ capstone-react-app/src/Components/APIUrlBase.js
@@ -1,2 +1,2 @@
 // Single area for the API URL. Change once here, changes everywhere.
-export const API_URL_BASE = 'http://localhost:8080/api/v1';
+export const API_URL_BASE = 'https://group2springfinal.herokuapp.com/';
